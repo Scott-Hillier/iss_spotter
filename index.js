e8c3e72618cf8d33ed1f80b379a76b6fc22f6f39
Scott Hillier <scotthhillier@gmail.com>
2021-11-04 18:55:53 -0700

    Completed

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('173.183.27.214', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
+
+// const coords = { latitude: '49.2514', longitude: '-123.0972' };
+
+// fetchISSFlyOverTimes(coords, (error, flyTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyover times:' , flyTimes);
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds`);
   }
-  console.log('It worked! Returned IP:' , ip);
-});
+};
 
-fetchCoordsByIP('173.183.27.214', (error, coordinates) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log('It worked! Returned coordinates:' , coordinates);
-});
-
-const coords = { latitude: '49.2514', longitude: '-123.0972' };
-
-fetchISSFlyOverTimes(coords, (error, flyTimes) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned flyover times:' , flyTimes);
+  printPassTimes(passTimes);
 });
